5f678338757fd757801990a4357f6e95d2de8a53
Yura <yuralvov754755@gmail.com>
2020-11-12 22:14:49 +0200

    redaction

--- js/main.js
+++ js/main.js
@@ -1,21 +1,17 @@
-const android = 15.678;
-const iphone = 123.965;
-const windowsPhone = 90.2345;
-const ans = Math.max(android , iphone , windowsPhone);
-console.log(ans);
-const ans1 = Math.min(android , iphone , windowsPhone);
-console.log(ans1);
-const totalPrice = (android + iphone + windowsPhone );
+const androidPhonePrice = 15.678;
+const iphonePhonePrice = 123.965;
+const windowsPhonePrice = 90.2345;
+const maxPrice = Math.max(androidPhonePrice , iphonePhonePrice , windowsPhonePrice);
+console.log(maxPrice);
+const minPrice = Math.min(androidPhonePrice , iphonePhonePrice , windowsPhonePrice);
+console.log(minPrice);
+const totalPrice = (androidPhonePrice + iphonePhonePrice + windowsPhonePrice );
 console.log(totalPrice);
-const ans3 = Math.round(totalPrice);
-console.log(ans3);
-const roundingPrice = 230;
-const roundengSum = Math.floor( android) + Math.floor(iphone) + Math.floor(windowsPhone);
+const avaragePrice = Math.round(totalPrice);
+console.log(avaragePrice);
+const roundengSum = Math.floor( androidPhonePrice) + Math.floor(iphonePhonePrice) + Math.floor(windowsPhonePrice);
 console.log(roundengSum);
-let underRounded = 228;
-underRounded = 228/100;
-const value = Math.round(underRounded);
-underRounded = value * 100;
+let underRounded = Math.round(roundengSum/100)*100;
 console.log(underRounded);
 if(underRounded%2 === 0){
     console.log(true);
@@ -23,32 +19,29 @@ if(underRounded%2 === 0){
     console.log(false);
 }
 const client = 500;
-    const surrender = Math.abs(underRounded-client);
-    console.log(surrender);
-const avarage = Math.floor(((iphone + android + windowsPhone)*100) / 3) / 100;
+const surrenderClient = Math.abs(roundengSum-client);
+console.log(surrenderClient);
+const avarage = Math.floor(((iphonePhonePrice + androidPhonePrice + windowsPhonePrice)*100) / 3) / 100;
 console.log(avarage);
-const random = getRandomInt;
-function getRandomInt(max) {
-    return Math.floor(Math.random() * Math.floor(max));
-  }
-  console.log(getRandomInt(100));
-  const price = 200;
-  const discountPercentage = 10 + Math.random() * (90 - 10 + 1);
-  let discount = Number(discountPercentage.toFixed(2)) * Math.floor(price) / 100;
-  console.log(discountPercentage);
-  const sum = Math.floor(price / 2 - discount);
-  console.log(sum);
+const randomSale = Math.floor(Math.random()*100) +1;
+console.log(randomSale);
+const clientSalePay = Number((totalPrice - (totalPrice / 100 * randomSale)).toFixed(2));
+console.log(clientSalePay);
+const PhoneCostPrice = totalPrice / 2;
+console.log(PhoneCostPrice);
+const clearSalary = Number((PhoneCostPrice - (totalPrice / 100 * randomSale)).toFixed(2));
+console.log(clearSalary); 
+
   console.log(
-      `максимальное число ${ans} 
-       минимальное число ${ans1}
+      `максимальное число ${maxPrice} 
+       минимальное число ${minPrice}
        вартість товарів  ${totalPrice}
-       вартість без копійок ${roundingPrice}
+       округленна вартість без копійок ${roundengSum}
        округленна сума до сотень ${underRounded}
-       булеве значення ${underRounded}
-       сума решти від вартості товару ${surrender}
+       булеве значення ${true}
+       сума решти від вартості товару ${surrenderClient}
        середнє значення цін ${avarage}
-       ціна товару ${price}
-       випадкова знижка ${discountPercentage}
-       результат після знижки ${sum}`
+       випадкова знижка ${randomSale}%
+       результат після знижки ${clearSalary}`
   );
 
